9fd93fead12a2d1973eda3ac34a628036af40d4b
Daniil Tsarenok <dants1596@mail.ru>
2020-11-27 09:57:42 +0500

    third
    third

---
+++ storage/framework/views/99aee2868d1b80c59ffa68657f97a97031a18ee7.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+<script src="<?php echo e(mix('js/app.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH /home/alexandr/PhpstormProjects/thirdtry/resources/views/spa.blade.php ENDPATH**/ ?>
